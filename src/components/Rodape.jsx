60776e6d6f5bb2aeb915f195a0b17cced9abefb9
gabriellgomess <gabriel.gomes@outlook.com>
2024-11-11 13:08:02 -0300

    add email no rodape

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -8,11 +8,12 @@ const Rodape = () => {
                 <Box sx={{width: {xs: '250px', sm: '250px', md: '318px', lg: '318px', xl: '318px'}}}>
                     <img width='100%' src={Logo} alt="Logo Monbank" />
                 </Box>                
-                <p className='text-white' style={{marginTop: '30px'}}>Copyright &copy; {new Date().getFullYear()}</p>
-                <p className='text-white' style={{marginTop: '30px'}}>Mon Seguros</p>
-                <p className='text-white' style={{marginTop: '30px'}}>CNPJ: 54.561.419/0001-41</p>
+                <p className='text-white' style={{marginTop: '20px'}}>Copyright &copy; {new Date().getFullYear()}</p>
+                <p className='text-white' style={{marginTop: '20px'}}>Mon Seguros</p>
+                <p className='text-white' style={{marginTop: '20px'}}>CNPJ: 54.561.419/0001-41</p>
                 <p className='text-white'>Rua Mostardeiro, no 322, salas 1501 e 1502 – CEP 90430-000</p>
                 <p className='text-white'>Moinhos de Vento – Porto Alegre/RS</p>
+                <p className='text-white'>atendimento@monseguros.com.br</p>
             </Box>
             <Box>
                 <p className='text-white'>Perguntas Frequentes</p>
